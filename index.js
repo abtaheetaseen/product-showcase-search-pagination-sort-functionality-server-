4abf80895984c19995326abd7e8bce4a0e1cb9d6
abtaheetaseen <abtahee.siddiqui@northsouth.edu>
2024-08-16 20:48:11 +0600

    fourth commit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 // mongodb
 // 2
 2
-34
+34555
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://showCase:yqnJWYojgbY8Lb2n@cluster1.ofi7kql.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1";
